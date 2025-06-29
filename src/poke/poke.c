5ea3a6b348b9ec54a81ecf85ab0b68dceb030d68
Hyeonhak Kim <gusgkr0117@korea.ac.kr>
2025-06-30 08:34:08 +0900

    Fix dlog_6

--- src/poke/poke.c
+++ src/poke/poke.c
@@ -24,6 +24,107 @@ int ibz_random_unit(ibz_t *q, const ibz_t *modulus) {
 }
 
 int ec_dlog_6(digit_t *scalarP, digit_t *scalarQ, ec_basis_t *base, ec_point_t *R, ec_curve_t *E) {
+    ec_basis_t three_base, two_base;
+    ec_point_t R3, R2;
+    digit_t scalarP3[NWORDS_ORDER], scalarQ3[NWORDS_ORDER];
+    digit_t scalarP2[NWORDS_ORDER], scalarQ2[NWORDS_ORDER];
+    ibz_t iP3, iQ3, iP2, iQ2, t1, t2, t3;
+
+    ibz_init(&iP3);
+    ibz_init(&iQ3);
+    ibz_init(&iP2);
+    ibz_init(&iQ2);
+    ibz_init(&t1);
+    ibz_init(&t2);
+    ibz_init(&t3);
+
+    copy_point(&R3, R);
+    copy_point(&three_base.P, &base->P);
+    copy_point(&three_base.Q, &base->Q);
+    copy_point(&three_base.PmQ, &base->PmQ);
+    for(int i = 0; i < POWER_OF_2; i++) {
+        ec_dbl(&R3, E, &R3);
+        ec_dbl(&three_base.P, E, &three_base.P);
+        ec_dbl(&three_base.Q, E, &three_base.Q);
+        ec_dbl(&three_base.PmQ, E, &three_base.PmQ);
+    }
+
+    ec_dlog_3(scalarP3, scalarQ3, &three_base, &R3, E);
+    ec_point_t test_point;
+    xDBLMUL(&test_point, &three_base.P, scalarP3, &three_base.Q, scalarQ3, &three_base.PmQ, E);
+    if (!ec_is_equal(&test_point, &R3)) {
+        printf("ec_dlog_3 fails\n");
+        return 0;
+    }
+
+    copy_point(&R2, R);
+    copy_point(&two_base.P, &base->P);
+    copy_point(&two_base.Q, &base->Q);
+    copy_point(&two_base.PmQ, &base->PmQ);
+    for(int i = 0; i < POWER_OF_3; i++) {
+        ec_mul_ibz(&R2, E, &ibz_const_three, &R2);
+        ec_mul_ibz(&two_base.P, E, &ibz_const_three, &two_base.P);
+        ec_mul_ibz(&two_base.Q, E, &ibz_const_three, &two_base.Q);
+        ec_mul_ibz(&two_base.PmQ, E, &ibz_const_three, &two_base.PmQ);
+    }
+
+    ec_dlog_2(scalarP2, scalarQ2, &two_base, &R2, E);
+    xDBLMUL(&test_point, &two_base.P, scalarP2, &two_base.Q, scalarQ2, &two_base.PmQ, E);
+    if (!ec_is_equal(&test_point, &R2)) {
+        printf("ec_dlog_2 fails\n");
+        return 0;
+    }
+
+    // Chinese Remainder Theorem
+    ibz_copy_digits(&iP3, scalarP3, NWORDS_ORDER);
+    ibz_copy_digits(&iQ3, scalarQ3, NWORDS_ORDER);
+    ibz_copy_digits(&iP2, scalarP2, NWORDS_ORDER);
+    ibz_copy_digits(&iQ2, scalarQ2, NWORDS_ORDER);
+
+    ibz_crt(&iP3, &iP3, &iP2, &TORSION_PLUS_3POWER, &TORSION_PLUS_2POWER);
+    ibz_crt(&iQ3, &iQ3, &iQ2, &TORSION_PLUS_3POWER, &TORSION_PLUS_2POWER);
+    ibz_to_digits(scalarP, &iP3);
+    ibz_to_digits(scalarQ, &iQ3);
+
+    // ibz_invmod(&t1, &TORSION_PLUS_2POWER, &TORSION_PLUS_3POWER);
+    // ibz_invmod(&t2, &TORSION_PLUS_3POWER, &TORSION_PLUS_2POWER);
+    // ibz_mul(&t3, &TORSION_PLUS_2POWER, &TORSION_PLUS_3POWER);
+
+    // // Compute scalarP
+    // ibz_mul(&iP3, &iP3, &t1);
+    // ibz_mul(&iP3, &iP3, &TORSION_PLUS_2POWER);
+    // ibz_mod(&iP3, &iP3, &t3);
+
+    // ibz_mul(&iP2, &iP2, &t2);
+    // ibz_mul(&iP2, &iP2, &TORSION_PLUS_3POWER);
+    // ibz_mod(&iP2, &iP2, &t3);
+
+    // ibz_add(&iP3, &iP3, &iP2);
+    // ibz_mod(&iP3, &iP3, &t3);
+    // ibz_to_digits(scalarP, &iP3);
+
+
+    // // Compute scalarQ
+    // ibz_mul(&iQ3, &iQ3, &t1);
+    // ibz_mul(&iQ3, &iQ3, &TORSION_PLUS_2POWER);
+    // ibz_mod(&iQ3, &iQ3, &t3);
+
+    // ibz_mul(&iQ2, &iQ2, &t2);
+    // ibz_mul(&iQ2, &iQ2, &TORSION_PLUS_3POWER);
+    // ibz_mod(&iQ2, &iQ2, &t3);
+
+    // ibz_add(&iQ3, &iQ3, &iQ2);
+    // ibz_mod(&iQ3, &iQ3, &t3);
+    // ibz_to_digits(scalarQ, &iQ3);
+
+    ibz_finalize(&t1);
+    ibz_finalize(&t2);
+    ibz_finalize(&t3);
+    ibz_finalize(&iP3);
+    ibz_finalize(&iQ3);
+    ibz_finalize(&iP2);
+    ibz_finalize(&iQ2);
+
     return 1;
 }
 
@@ -74,8 +175,21 @@ int eval_dimtwo_isog(theta_chain_t *phi, ec_basis_t *evalPQ, ec_basis_t *PQ, the
     lift_basis(&jacR, &jacS, &RS, &E01->E2);
 
     ec_dlog_6(x, y, &imRS_basis, &imP, &phi->codomain.E1);
+    ec_point_t test_point;
+    xDBLMUL(&test_point, &imRS_basis.P, x, &imRS_basis.Q, y, &imRS_basis.PmQ, &phi->codomain.E1);
+    if (!ec_is_equal(&test_point, &imP)) {
+        printf("x*R + y*S != imP\n");
+        return 0;
+    }
+
     DBLMUL_generic(&evalP, &jacR, x, &jacS, y, &E01->E2, NWORDS_FIELD);
     ec_dlog_6(x, y, &imRS_basis, &imQ, &phi->codomain.E1);
+    xDBLMUL(&test_point, &imRS_basis.P, x, &imRS_basis.Q, y, &imRS_basis.PmQ, &phi->codomain.E1);
+    if (!ec_is_equal(&test_point, &imQ)) {
+        printf("x*R + y*S != imQ\n");
+        return 0;
+    }
+
     DBLMUL_generic(&evalQ, &jacR, x, &jacS, y, &E01->E2, NWORDS_FIELD);
     ADD(&evalPmQ, &evalP, &evalQ, &E01->E2);
     
@@ -120,7 +234,7 @@ int test() {
     ec_curve_t curve = CURVE_E0;
     fp2_t j_inv;
     ec_j_inv(&j_inv, &curve);
-    fp2_print("j_invariant",&j_inv);
+    // fp2_print("j_invariant",&j_inv);
 
     quat_alg_elem_t tau;
     quat_alg_elem_init(&tau);
@@ -134,8 +248,8 @@ int test() {
     ibq_t tau_norm;
     ibq_init(&tau_norm);
     quat_alg_norm(&tau_norm, &tau, &QUATALG_PINFTY);
-    gmp_printf("nrd(tau) : %Qd\n", tau_norm);
-    gmp_printf("rhs : %Zd\n", rhs);
+    // gmp_printf("nrd(tau) : %Qd\n", tau_norm);
+    // gmp_printf("rhs : %Zd\n", rhs);
 
     ec_basis_t E0_two, E0_three;
     copy_point(&E0_two.P, &BASIS_EVEN.P);
@@ -145,10 +259,10 @@ int test() {
     copy_point(&E0_three.Q, &BASIS_THREE.Q);
     copy_point(&E0_three.PmQ, &BASIS_THREE.PmQ);
 
-    point_print("P2", E0_two.P);
-    point_print("Q2", E0_two.Q);
-    point_print("P3", E0_three.P);
-    point_print("Q3", E0_three.Q);
+    // point_print("P2", E0_two.P);
+    // point_print("Q2", E0_two.Q);
+    // point_print("P3", E0_three.P);
+    // point_print("Q3", E0_three.Q);
 
     // ibz_set(&(tau.coord[0]), 0);
     // ibz_set(&(tau.coord[1]), 0);
@@ -160,10 +274,10 @@ int test() {
     endomorphism_application_three_basis(&E0_three, &curve, &tau, TORSION_PLUS_ODD_POWERS[0]);
     endomorphism_application_even_basis(&E0_two, &curve, &tau, TORSION_PLUS_EVEN_POWER);
 
-    point_print("tau(P2): ", E0_two.P);
-    point_print("tau(Q2): ", E0_two.Q);
-    point_print("tau(P3): ", E0_three.P);
-    point_print("tau(Q3): ", E0_three.Q);
+    // point_print("tau(P2): ", E0_two.P);
+    // point_print("tau(Q2): ", E0_two.Q);
+    // point_print("tau(P3): ", E0_three.P);
+    // point_print("tau(Q3): ", E0_three.Q);
     // ec_point_t tmpP, tmpQ;
     // ec_mul_ibz(&tmpP, &curve, &ibz_const_five, &BASIS_THREE.P);
     // ec_mul_ibz(&tmpQ, &curve, &ibz_const_five, &BASIS_THREE.Q);
@@ -260,10 +374,17 @@ int test() {
 
     // Point evaluation via 2-dim isogeny
     jac_point_t P, Q, R, S, X, Y;
-    lift_basis(&P, &Q, &BASIS_EVEN, &curve);
-    lift_basis(&R, &S, &BASIS_THREE, &curve);
-    lift_basis(&X, &Y, &BASIS_FIVE, &curve);
+    copy_point(&E0_two.P, &BASIS_EVEN.P);
+    copy_point(&E0_two.Q, &BASIS_EVEN.Q);
+    copy_point(&E0_two.PmQ, &BASIS_EVEN.PmQ);
+    copy_point(&E0_three.P, &BASIS_THREE.P);
+    copy_point(&E0_three.Q, &BASIS_THREE.Q);
+    copy_point(&E0_three.PmQ, &BASIS_THREE.PmQ);
 
+    lift_basis(&P, &Q, &E0_two, &curve);
+    lift_basis(&R, &S, &E0_three, &curve);
+
+    // lift_basis(&X, &Y, &BASIS_FIVE, &curve);
     // P23x = P + R + X and Q23x = Q + S + Y
     jac_point_t P23x, Q23x, PmQ23x;
     ADD(&P23x, &P, &R, &curve);
