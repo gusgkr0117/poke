337ff84c5ced2449f1ba5e3c21b12179a012c0d1
Hyeonhak Kim <gusgkr0117@korea.ac.kr>
2025-08-29 09:20:06 +0900

    update for inke kem

--- src/poke/ref/inkex/test/inke-test.c
+++ src/poke/ref/inkex/test/inke-test.c
@@ -4,6 +4,50 @@
 #include <rng.h>
 #include <bench.h>
 
+int test_inke_kem(int bench_loops) {
+    inke_sk_t sk = {0};
+    inke_pk_t pk = {0};
+    inke_ct_t ct = {0};
+    unsigned char encap_key[32] = {0};
+    unsigned char decap_key[32] = {0};
+    size_t m_len = 0;
+    uint64_t cycles1, cycles2;
+    uint64_t cycle_runs[3] = {0};
+    uint8_t s[32] = {0};
+
+    for(int i = 0; i < bench_loops; i++) {
+        cycles1 = cpucycles();
+        randombytes(s, 32);
+        keygen(&sk, &pk);
+        cycles2 = cpucycles();
+        cycle_runs[0] += cycles2 - cycles1;
+        
+        cycles1 = cpucycles();
+        encaps(encap_key, &ct, &pk);
+        cycles2 = cpucycles();
+        cycle_runs[1] += cycles2 - cycles1;
+
+        cycles1 = cpucycles();
+        decaps(decap_key, &ct, &pk, &sk, s);
+        cycles2 = cpucycles();
+        cycle_runs[2] += cycles2 - cycles1;
+
+        for (int j = 0; j < sizeof(encap_key); j++) {
+            if (encap_key[j] != decap_key[j]) return 1;
+        }
+    }
+
+    printf("test loops : %d\n", bench_loops);
+    printf("  gen takes .................................... %.6f %s\n",
+            (double)(cycle_runs[0])/(bench_loops), BENCH_UNITS);
+    printf("  ecaps takes .................................... %.6f %s\n",
+            (double)(cycle_runs[1])/(bench_loops), BENCH_UNITS);
+    printf("  decaps takes .................................... %.6f %s\n",
+            (double)(cycle_runs[2])/(bench_loops), BENCH_UNITS);
+
+    return 0;
+}
+
 int test_inke(int bench_loops) {
     inke_sk_t sk = {0};
     inke_pk_t pk = {0};
@@ -22,7 +66,7 @@ int test_inke(int bench_loops) {
         cycle_runs[0] += cycles2 - cycles1;
         
         cycles1 = cpucycles();
-        encrypt(&ct, &pk, m, 32);
+        encrypt(&ct, &pk, m, 32, NULL, 0);
         cycles2 = cpucycles();
         cycle_runs[1] += cycles2 - cycles1;
 
@@ -52,5 +96,6 @@ int main(int argc, char* argv[]) {
     if (argc > 1) {
         loops = atoi(argv[1]);
     }
-    return test_inke(loops);
+    // return test_inke(loops);
+    return test_inke_kem(loops);
 } 
