4da5a4c12f067dab4c636d1ce5024e9e33aa987e
Hyeonhak Kim <gusgkr0117@korea.ac.kr>
2025-09-15 09:23:54 +0900

    separate kem and pke test

--- src/poke/ref/pokex/test/poke-test.c
+++ src/poke/ref/pokex/test/poke-test.c
@@ -4,50 +4,6 @@
 #include <rng.h>
 #include <bench.h>
 
-int test_poke_kem(int bench_loops) {
-    poke_sk_t sk = {0};
-    poke_pk_t pk = {0};
-    poke_ct_t ct = {0};
-    unsigned char encap_key[32] = {0};
-    unsigned char decap_key[32] = {0};
-    size_t m_len = 0;
-    uint64_t cycles1, cycles2;
-    uint64_t cycle_runs[3] = {0};
-    uint8_t s[32] = {0};
-
-    for(int i = 0; i < bench_loops; i++) {
-        cycles1 = cpucycles();
-        randombytes(s, 32);
-        keygen(&sk, &pk);
-        cycles2 = cpucycles();
-        cycle_runs[0] += cycles2 - cycles1;
-        
-        cycles1 = cpucycles();
-        encaps(encap_key, &ct, &pk);
-        cycles2 = cpucycles();
-        cycle_runs[1] += cycles2 - cycles1;
-
-        cycles1 = cpucycles();
-        decaps(decap_key, &ct, &pk, &sk, s);
-        cycles2 = cpucycles();
-        cycle_runs[2] += cycles2 - cycles1;
-
-        for (int j = 0; j < sizeof(encap_key); j++) {
-            if (encap_key[j] != decap_key[j]) return 1;
-        }
-    }
-
-    printf("test loops : %d\n", bench_loops);
-    printf("  gen takes .................................... %.6f %s\n",
-            (double)(cycle_runs[0])/(bench_loops), BENCH_UNITS);
-    printf("  ecaps takes .................................... %.6f %s\n",
-            (double)(cycle_runs[1])/(bench_loops), BENCH_UNITS);
-    printf("  decaps takes .................................... %.6f %s\n",
-            (double)(cycle_runs[2])/(bench_loops), BENCH_UNITS);
-
-    return 0;
-}
-
 int test_poke(int bench_loops) {
     poke_sk_t sk = {0};
     poke_pk_t pk = {0};
@@ -96,5 +52,5 @@ int main(int argc, char* argv[]) {
     if (argc > 1) {
         loops = atoi(argv[1]);
     }
-    return test_poke_kem(loops);
+    return test_poke(loops);
 } 
